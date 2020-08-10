9981c85782790f71fc394a9f74b043f0ee2dcc03
QingYuAnWayne <zhangxuwei@buaa.edu.cn>
2020-08-10 19:07:40 +0800

    删除了修改前的文件

--- SmallSemester/SmallSemesterChild/models.py
+++ SmallSemester/SmallSemesterChild/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
 class Test(models.Model):
     name = models.CharField(max_length=20)
 
