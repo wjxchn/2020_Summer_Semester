69cdc0a09c63038e1d13a8ef68e08999bf0b23f8
QingYuAnWayne <zhangxuwei@buaa.edu.cn>
2020-08-11 18:46:55 +0800

    团队页面更新

--- SmallSemester/SmallSemesterVue/src/components/Team.vue
+++ SmallSemester/SmallSemesterVue/src/components/Team.vue
@@ -2,38 +2,72 @@
     <div>
         <Guider id="navBar" :class="{isFixed:istabBar}"/>
         <main id="mainPart" role="main" class="container">
-            <el-row>
-                <i class="el-icon-user-solid"></i>
-                  <el-button type="text">我的团队</el-button>
-            </el-row>
+            <br>
+            <el-button type="primary" style="float:right" icon="el-icon-s-flag" @click="open">创建团队</el-button>
+            <!-- 
+             <el-dialog title="创建团队" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+                <el-form-item label="团队名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+            </el-form>
+
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+
+
+
+            </div>
+            </el-dialog> -->
+
+
+
+
+
+
+            <h5 fixed="right"> 我的团队 </h5>
+            
             <el-table
             :data="tableData"
             style="width: 100%">
             <el-table-column
                 prop="name"
-                label="组名"
-                width="180">
+                label="团队名"
+                width="180"
+                fixed="left"
+                align="center">
+
             </el-table-column>
             <el-table-column
                 prop="owner"
-                label="组长"
-                width="180">
+                label="管理员"
+                width="180"
+                fixed="left" 
+                align="center">
             </el-table-column>
             <el-table-column
-                prop="time"
-                label="创建时间">
+                prop="number"
+                label="人数"
+                width="180"
+                align="center"
+                fixed="left">
             </el-table-column>
-            <el-table-column label="操作">
-            <template slot-scope="scope">
-                <el-button
-                size="mini">查看</el-button>
-                <el-button
-                size="mini"
-                type="danger">退出</el-button>
-            </template>
+            <el-table-column
+                label="操作"
+                align="center"
+                width="150"
+                fixed="right"
+             >
+           <template 
+                slot-scope="scope"
+                >
+                    
+                        <el-button @click="handleClick(scope.row)" type="primary">进入团队空间</el-button>
+                    
+                    </template>
             </el-table-column>
             </el-table>     
-            <el-button type="primary">创建团队</el-button>
         </main>
         <BottomGuider/>
     </div>
@@ -53,24 +87,49 @@ export default {
             tableData: [{
                 name: '组A',
                 owner: '王小虎',
-                time: '2020/8/20'
+                number:'100'
             }, {
                 name: '组B',
                 owner: '王小虎',
-                time: '2020/8/20'
+                number:'100'
             }, {
                 name: '组C',
                 owner: '王小虎',
-                time: '2020/8/20'
+                number:'100'
             }, {
                 name: '组D',
                 owner: '王小虎',
-                time: '2020/8/20'
+                number:'1000000000000000000'
             }],
-            istabBar: false
-        }
+            istabBar: false,
+            dialogFormVisible: false,
+            dialogFormVisible2: false,
+            form: {
+            name: '',
+            
+            },
+            formLabelWidth: '120px'
+            }
     },
     methods: {
+        open(){
+            this.$prompt('请输入团队名','提示',{
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                inputPattern:/^[\u4E00-\u9FA5A-Za-z0-9_]+.{1,}$/,
+                inputErrorMessage: '团队名格式错误'
+            }).then(({ value }) =>{
+                this.$message({
+                    type:'success',
+                    message:'创建成功！ 你的团队名是：' + value
+                });
+            }).catch(() => {
+                this.$message({
+                    type:'info',
+                    message:'取消输入'
+                });
+            });
+        },
         // 添加一个方法 兼容
         handleScroll () {
             let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
