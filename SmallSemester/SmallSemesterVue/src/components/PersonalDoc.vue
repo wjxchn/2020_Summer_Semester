cbc0e6dde54e6a6a4793c1d02898f50d0e5802ad
Gavin_lrn <54489982+gavin-lren@users.noreply.github.com>
2020-08-12 14:58:42 +0800

    Update PersonalDoc.vue

--- SmallSemester/SmallSemesterVue/src/components/PersonalDoc.vue
+++ SmallSemester/SmallSemesterVue/src/components/PersonalDoc.vue
@@ -1,8 +1,44 @@
 <template>
     <div>
         <Guider id="navBar" :class="{isFixed:istabBar}"/>
+        <el-container>
+            <el-aside  width="210px">
+
+            <!--     ↓↓↓↓↓↓↓↓↓↓↓↓↓↓  这个default-active是指当前激活的页面，把页面对应的index写进去，例如：当前页面是我的文档，则写进去2-1 -->
+            <el-menu default-active="2-1" class="el-menu-vertical-demo"
+            @open="handleOpen" @close="handleClose" background-color="#fff"
+            text-color="#000000" active-text-color="#f96332" >
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>主页</span>
+                </template>
+                    <el-menu-item index="1-1" @click="Home" :loading="logining" ><i class="el-icon-house" style="color:black"></i>首页</el-menu-item>
+                    <el-menu-item index="1-2" @click="Person" :loading="logining"><i class="el-icon-user" style="color:black"></i>个人资料</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>工作台</span>
+                </template>
+                    <el-menu-item index="2-1" @click="Mywork" :loading="logining"><i class="el-icon-notebook-2" style="color:black"></i>我的文档</el-menu-item>
+                    <el-menu-item index="2-2" @click="Myteam" :loading="logining"><i class="el-icon-connection" style="color:black"></i>我的团队</el-menu-item>
+                    <el-menu-item index="2-3" @click="Trash" :loading="logining"><i class="el-icon-delete" style="color:black"></i>回收站</el-menu-item>
+                    <el-menu-item index="2-4" @click="Message" :loading="logining"><i class="el-icon-chat-dot-round" style="color:black"></i>收件箱</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>其他</span>
+                </template>
+                    <el-menu-item index="3-1" @click="Help" :loading="logining"><i class="el-icon-view" style="color:black"></i>帮助</el-menu-item>
+                    <el-menu-item index="3-2" @click="Aboutus" :loading="logining"><i class="el-icon-link" style="color:black"></i>开发者信息</el-menu-item>
+            </el-submenu>
+            </el-menu>
+            </el-aside>
+        <el-container>
         <main id="mainPart" role="main" class="container">
-            <div class="personaldoc">
+            <div class="personaldoc" >
                 <div style="float:right">
                 <el-input  placeholder="请输入内容" style="width:250px">
                 <el-button  slot="append" icon="el-icon-search"></el-button></el-input>
@@ -88,6 +124,8 @@
                 </el-pagination>
             </div>
         </main>
+        </el-container>
+        </el-container>
         <BottomGuider/>
     </div>
 </template>
@@ -127,6 +165,37 @@ export default {
         }
     },
     methods: {
+        //侧边栏的跳转
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        Home(){
+            this.$router.push('/');
+        },
+        Person(){
+            this.$router.push('/Person');
+        },
+        Mywork(){
+            this.$router.push('/Personaldoc');
+        },
+        Myteam(){
+            this.$router.push('/Team');
+        },
+        Trash(){
+            this.$router.push('/Trash');
+        },
+        Message(){
+            this.$router.push('/Message');
+        },
+        Help(){
+            this.$router.push('/Help');
+        },
+        Aboutus(){
+            this.$router.push('/Aboutus');
+        },
         // 添加一个方法 兼容
         handleScroll () {
             let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
@@ -221,7 +290,7 @@ p.history {
 }
 #navBar {
     width: 100%;
-    background-color: rgba(255, 255, 255, 0.15);
+    background-color: #f96332;
     backdrop-filter: blur(30px);
 }
 #mainPart {
@@ -232,5 +301,8 @@ p.history {
 }
 .el-table .success-row {
     background: #f0f9eb;
+}
+.as{
+    width:20px;
 }
 </style>
