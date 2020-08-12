43acd115ffa50ac6ad558e8dce1b789956980906
Gavin_lrn <54489982+gavin-lren@users.noreply.github.com>
2020-08-12 15:00:24 +0800

    Update Team.vue

--- SmallSemester/SmallSemesterVue/src/components/Team.vue
+++ SmallSemester/SmallSemesterVue/src/components/Team.vue
@@ -1,74 +1,78 @@
 <template>
     <div>
         <Guider id="navBar" :class="{isFixed:istabBar}"/>
+        <el-container>
+            <el-aside width="210px">
+
+            <!--     ↓↓↓↓↓↓↓↓↓↓↓↓↓↓  这个default-active是指当前激活的页面，把页面对应的index写进去，例如：当前页面是我的文档，则写进去2-1 -->
+            <el-menu default-active="2-2" class="el-menu-vertical-demo"
+            @open="handleOpen" @close="handleClose" background-color="#545c64"
+            text-color="#fff" active-text-color="#ffd04b" >
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>主页</span>
+                </template>
+                    <el-menu-item index="1-1" @click="Home" :loading="logining" ><i class="el-icon-house"></i>首页</el-menu-item>
+                    <el-menu-item index="1-2" @click="Person" :loading="logining"><i class="el-icon-user"></i>个人资料</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>工作台</span>
+                </template>
+                    <el-menu-item index="2-1" @click="Mywork" :loading="logining"><i class="el-icon-notebook-2"></i>我的文档</el-menu-item>
+                    <el-menu-item index="2-2" @click="Myteam" :loading="logining"><i class="el-icon-connection"></i>我的团队</el-menu-item>
+                    <el-menu-item index="2-3" @click="Trash" :loading="logining"><i class="el-icon-delete"></i>回收站</el-menu-item>
+                    <el-menu-item index="2-4" @click="Message" :loading="logining"><i class="el-icon-chat-dot-round"></i>收件箱</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title">
+                    <i class="el-icon-copy-document"></i>
+                    <span>其他</span>
+                </template>
+                    <el-menu-item index="3-1" @click="Help" :loading="logining"><i class="el-icon-view"></i>帮助</el-menu-item>
+                    <el-menu-item index="3-2" @click="Aboutus" :loading="logining"><i class="el-icon-link"></i>开发者信息</el-menu-item>
+            </el-submenu>
+            </el-menu>
+            </el-aside>
+        <el-container>
         <main id="mainPart" role="main" class="container">
-            <br>
-            <el-button type="primary" style="float:right" icon="el-icon-s-flag" @click="open">创建团队</el-button>
-            <!-- 
-             <el-dialog title="创建团队" :visible.sync="dialogFormVisible">
-            <el-form :model="form">
-                <el-form-item label="团队名称" :label-width="formLabelWidth">
-                    <el-input v-model="form.name" autocomplete="off"></el-input>
-                </el-form-item>
-            </el-form>
-
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-
-
-
-            </div>
-            </el-dialog> -->
-
-
-
-
-
-
-            <h5 fixed="right"> 我的团队 </h5>
-            
+            <el-row>
+                <i class="el-icon-user-solid"></i>
+                  <el-button type="text">我的团队</el-button>
+            </el-row>
             <el-table
             :data="tableData"
             style="width: 100%">
             <el-table-column
                 prop="name"
-                label="团队名"
-                width="180"
-                fixed="left"
-                align="center">
-
+                label="组名"
+                width="180">
             </el-table-column>
             <el-table-column
                 prop="owner"
-                label="管理员"
-                width="180"
-                fixed="left" 
-                align="center">
+                label="组长"
+                width="180">
             </el-table-column>
             <el-table-column
-                prop="number"
-                label="人数"
-                width="180"
-                align="center"
-                fixed="left">
+                prop="time"
+                label="创建时间">
             </el-table-column>
-            <el-table-column
-                label="操作"
-                align="center"
-                width="150"
-                fixed="right"
-             >
-           <template 
-                slot-scope="scope"
-                >
-                    
-                        <el-button @click="handleClick(scope.row)" type="primary">进入团队空间</el-button>
-                    
-                    </template>
+            <el-table-column label="操作">
+            <template slot-scope="scope">
+                <el-button
+                size="mini">查看</el-button>
+                <el-button
+                size="mini"
+                type="danger">退出</el-button>
+            </template>
             </el-table-column>
             </el-table>     
+            <el-button type="primary">创建团队</el-button>
         </main>
+        </el-container>
+        </el-container>
         <BottomGuider/>
     </div>
 </template>
@@ -87,48 +91,53 @@ export default {
             tableData: [{
                 name: '组A',
                 owner: '王小虎',
-                number:'100'
+                time: '2020/8/20'
             }, {
                 name: '组B',
                 owner: '王小虎',
-                number:'100'
+                time: '2020/8/20'
             }, {
                 name: '组C',
                 owner: '王小虎',
-                number:'100'
+                time: '2020/8/20'
             }, {
                 name: '组D',
                 owner: '王小虎',
-                number:'1000000000000000000'
+                time: '2020/8/20'
             }],
-            istabBar: false,
-            dialogFormVisible: false,
-            dialogFormVisible2: false,
-            form: {
-            name: '',
-            
-            },
-            formLabelWidth: '120px'
-            }
+            istabBar: false
+        }
     },
     methods: {
-        open(){
-            this.$prompt('请输入团队名','提示',{
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                inputPattern:/^[\u4E00-\u9FA5A-Za-z0-9_]+.{1,}$/,
-                inputErrorMessage: '团队名格式错误'
-            }).then(({ value }) =>{
-                this.$message({
-                    type:'success',
-                    message:'创建成功！ 你的团队名是：' + value
-                });
-            }).catch(() => {
-                this.$message({
-                    type:'info',
-                    message:'取消输入'
-                });
-            });
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        Home(){
+            this.$router.push('/');
+        },
+        Person(){
+            this.$router.push('/Person');
+        },
+        Mywork(){
+            this.$router.push('/Personaldoc');
+        },
+        Myteam(){
+            this.$router.push('/Team');
+        },
+        Trash(){
+            this.$router.push('/Trash');
+        },
+        Message(){
+            this.$router.push('/Message');
+        },
+        Help(){
+            this.$router.push('/Help');
+        },
+        Aboutus(){
+            this.$router.push('/Aboutus');
         },
         // 添加一个方法 兼容
         handleScroll () {
