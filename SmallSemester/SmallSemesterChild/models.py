65b5fe8652605c976c6667fd37a6447f38c786fb
wjxchn <1822630963@qq.com>
2020-08-15 19:36:39 +0800

    内容更新

---
+++ SmallSemester/SmallSemesterChild/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils import timezone
+import datetime
+class Test(models.Model):
+    name = models.CharField(max_length=20)
+
+class Test2(models.Model):
+    name = models.CharField(max_length=20)
+
+class Test3(models.Model):
+    age = models.IntegerField(null=True,blank=True)
+    phonenumber = models.CharField(max_length=200,null=True,blank=True)
+
+class Plain(models.Model):
+    plainname = models.TextField()
+
+class Group(models.Model):
+    creater = models.TextField()  #foreign
+    groupid = models.IntegerField(primary_key = True , default = 0)
+    time = models.DateField(auto_now = True)
+    group_name = models.TextField()
+    introduction = models.TextField()
+
+class Document(models.Model):
+    doc_name = models.TextField()
+    doc_id = models.IntegerField(primary_key = True , default = 0)
+    doc_content = models.TextField()
+    introduction = models.TextField()
+    time = models.DateTimeField(auto_now = True)
+    doc_groupid = models.IntegerField(null=True,blank=True)   #foreign
+    doc_creater = models.TextField(null=True)   #foreign
+    isin_recycle = models.BooleanField()
+
+class Comment(models.Model):
+    com_content = models.TextField()
+    time = models.DateTimeField(auto_now = True)
+    com_author = models.TextField()  #foreign
+    com_id = models.IntegerField(primary_key = True , default = 0) 
+    doc_id = models.IntegerField()     #foreign
+
+class Demo(models.Model):
+    demo_id = models.IntegerField(primary_key = True , default = 0) 
+    demo_content = models.TextField()
+
+class Browse(models.Model):
+    browse_time = models.DateTimeField(auto_now = True)
+    browse_id = models.IntegerField(primary_key = True , default = 0) 
+    username = models.TextField()   #foreign
+    doc_id = models.IntegerField()    #foreign
+
+class Belong(models.Model):
+    group_id = models.IntegerField()   #foreign
+    username = models.TextField()   #foreign
+
+class Docbelong(models.Model):
+    doc_id = models.IntegerField()      #foreign
+    group_id = models.IntegerField()   #foreign
+
+class Favorite(models.Model):
+    doc_id = models.IntegerField()      #foreign
+    username = models.TextField()   #foreign
+
+class UserExtension(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='extension')
+    name = models.TextField(default = '暂无')
+    sex = models.TextField(default = '不公开')
+    birthday = models.DateField(default=timezone.now)
+    selfintro = models.TextField(default = '暂无个人简介')
+
+@receiver(post_save,sender=User)
+def create_user_extension(sender,instance,created,**kwargs):
+    if created:
+        UserExtension.objects.create(user=instance)
+    else:
+        instance.extension.save()
